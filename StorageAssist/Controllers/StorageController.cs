b308ab914f38ffaca957c9b58722b63646a1573e
Michał Stadryniak <stadryniak.michal@gmail.com>
2019-10-05 14:22:47 +0200

    Added to navbar Add panel - WIP

---
+++ StorageAssist/Controllers/StorageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+
+namespace StorageAssist.Controllers
+{
+    public class StorageController : Controller
+    {
+        // GET
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string AddStorage()
+        {
+            return "Test";
+            //return RedirectToAction("Index");
+        }
+    }
+}
