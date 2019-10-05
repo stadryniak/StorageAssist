8bcc0730c6d0fc4f931aa27015fb01903f5db057
Michał Stadryniak <stadryniak.michal@gmail.com>
2019-10-05 14:13:44 +0200

    Initial commit Database model and startup done

---
+++ StorageAssist/Migrations/20191005120458_init.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StorageAssist.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUsers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(maxLength: 256, nullable: true),
+                    Email = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<bool>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CommonResources",
+                columns: table => new
+                {
+                    CommonResourceId = table.Column<string>(nullable: false),
+                    OwnerId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommonResources", x => x.CommonResourceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<string>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(maxLength: 128, nullable: false),
+                    ProviderKey = table.Column<string>(maxLength: 128, nullable: false),
+                    ProviderDisplayName = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    RoleId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    LoginProvider = table.Column<string>(maxLength: 128, nullable: false),
+                    Name = table.Column<string>(maxLength: 128, nullable: false),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    NoteId = table.Column<string>(nullable: false),
+                    CommonResourceId = table.Column<string>(nullable: true),
+                    OwnerId = table.Column<string>(nullable: false),
+                    NoteName = table.Column<string>(nullable: true),
+                    NoteType = table.Column<string>(nullable: true),
+                    NoteText = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.NoteId);
+                    table.ForeignKey(
+                        name: "FK_Notes_CommonResources_CommonResourceId",
+                        column: x => x.CommonResourceId,
+                        principalTable: "CommonResources",
+                        principalColumn: "CommonResourceId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Storages",
+                columns: table => new
+                {
+                    StorageId = table.Column<string>(nullable: false),
+                    CommonResourceId = table.Column<string>(nullable: true),
+                    OwnerId = table.Column<string>(nullable: false),
+                    StorageName = table.Column<string>(nullable: false),
+                    StorageType = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Storages", x => x.StorageId);
+                    table.ForeignKey(
+                        name: "FK_Storages_CommonResources_CommonResourceId",
+                        column: x => x.CommonResourceId,
+                        principalTable: "CommonResources",
+                        principalColumn: "CommonResourceId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserCommonResource",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    CommonResourceId = table.Column<string>(nullable: false),
+                    UserCommonResourceId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCommonResource", x => new { x.UserId, x.CommonResourceId });
+                    table.UniqueConstraint("AK_UserCommonResource_UserCommonResourceId", x => x.UserCommonResourceId);
+                    table.ForeignKey(
+                        name: "FK_UserCommonResource_CommonResources_CommonResourceId",
+                        column: x => x.CommonResourceId,
+                        principalTable: "CommonResources",
+                        principalColumn: "CommonResourceId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserCommonResource_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<string>(nullable: false),
+                    StorageId = table.Column<string>(nullable: true),
+                    Type = table.Column<int>(nullable: false),
+                    ProductName = table.Column<string>(nullable: false),
+                    QuantityType = table.Column<int>(nullable: false),
+                    QuantityCount = table.Column<int>(nullable: true),
+                    QuantityWeight = table.Column<float>(nullable: true),
+                    BuyDate = table.Column<DateTime>(nullable: false, defaultValueSql: "getdate()"),
+                    ExpirationDate = table.Column<DateTime>(nullable: true),
+                    Comment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Products_Storages_StorageId",
+                        column: x => x.StorageId,
+                        principalTable: "Storages",
+                        principalColumn: "StorageId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_CommonResourceId",
+                table: "Notes",
+                column: "CommonResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_StorageId",
+                table: "Products",
+                column: "StorageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Storages_CommonResourceId",
+                table: "Storages",
+                column: "CommonResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCommonResource_CommonResourceId",
+                table: "UserCommonResource",
+                column: "CommonResourceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "Notes");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "UserCommonResource");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "Storages");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "CommonResources");
+        }
+    }
+}
---
+++ StorageAssist/Models/DatabaseModel.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace StorageAssist.Models
+{
+
+    public class AppUserContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppUserContext( DbContextOptions<AppUserContext> options )
+            : base( options )
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<CommonResource> CommonResources { get; set; }
+        public DbSet<UserCommonResource> UserCommonResources { get; set; }
+
+        protected override void OnModelCreating( ModelBuilder modelBuilder )
+        {
+            //many-to-many relationship between User-CommonResource
+            modelBuilder.Entity<UserCommonResource>()
+                .HasKey( uc => new { uc.UserId, uc.CommonResourceId } );
+            modelBuilder.Entity<UserCommonResource>()
+                .HasOne( uc => uc.User )
+                .WithMany( u => u.UserCommonResource )
+                .HasForeignKey( uc => uc.UserId );
+            modelBuilder.Entity<UserCommonResource>()
+                .HasOne( uc => uc.CommonResource )
+                .WithMany( c => c.UserCommonResource )
+                .HasForeignKey( uc => uc.CommonResourceId );
+
+            //UserCommonResource (join entity) unique key
+            modelBuilder.Entity<UserCommonResource>()
+                .HasAlternateKey( uc => uc.UserCommonResourceId );
+
+            //Product buyDate defaults to current day and time
+            modelBuilder.Entity<Product>()
+                .Property( p => p.BuyDate )
+                .HasDefaultValueSql( "getdate()" );
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public enum StorageType
+    {
+        Fridge, Freezer, Other
+    }
+
+    public enum ProductType
+    {
+        Meat, Vegetables, Fruits, Dairy, Candy, Other
+    }
+
+    public enum QuantityType
+    {
+        Count, Weight
+    }
+
+    [Table( "Users" )]
+    public class ApplicationUser : IdentityUser
+    {
+        //list of join entities (many-to-many)
+        public ApplicationUser()
+        {
+            UserCommonResource=new List<UserCommonResource>();
+        }
+        public List<UserCommonResource> UserCommonResource { get; set; }
+    }
+
+    [Table( "CommonResources" )]
+    public class CommonResource
+    {
+        public CommonResource()
+        {
+            UserCommonResource=new List<UserCommonResource>();
+            Storages=new List<Storage>();
+            Notes=new List<Note>();
+        }
+        //key
+        public string CommonResourceId { get; set; }
+        //list of join entities (many-to-many)
+        public List<UserCommonResource> UserCommonResource { get; set; }
+        public List<Storage> Storages { get; set; }
+        public List<Note> Notes { get; set; }
+        //Use to (dis)allow delete
+        [Required]
+        public string OwnerId { get; set; }
+    }
+
+    [Table( "UserCommonResource" )]
+    public class UserCommonResource
+    {
+        public string UserCommonResourceId { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public string CommonResourceId { get; set; }
+        public CommonResource CommonResource { get; set; }
+    }
+
+    [Table( "Notes" )]
+    public class Note
+    {
+        public string NoteId { get; set; }
+        public string CommonResourceId { get; set; }
+        public CommonResource CommonResource { get; set; }
+        //use to (dis)allow delete
+        [Required]
+        public string OwnerId { get; set; }
+        public string NoteName { get; set; }
+        public string NoteType { get; set; }
+        public string NoteText { get; set; }
+    }
+
+    /// <summary>
+    /// Contains list of products, type of storage, commonResource to which it belongs.
+    /// </summary>
+    [Table( "Storages" )]
+    public class Storage
+    {
+        public Storage()
+        {
+            Products=new List<Product>();
+        }
+        public string StorageId { get; set; }
+
+        //Foreign key, common resource containing storage
+        public string CommonResourceId { get; set; }
+        public CommonResource CommonResource { get; set; }
+        //use to (dis)allow delete
+        [Required]
+        public string OwnerId { get; set; }
+        //name to present in ui
+        [Required]
+        public string StorageName { get; set; }
+        public StorageType StorageType { get; set; }
+        public List<Product> Products { get; set; }
+    }
+
+    /// <summary>
+    /// Contains information about storage, quantity, type, expiration date, buy date, etc.
+    /// </summary>
+    [Table( "Products" )]
+    public class Product
+    {
+        //Primary key
+        public string ProductId { get; set; }
+
+        //Foreign key, storage containing product
+        public string StorageId { get; set; }
+        public Storage Storage { get; set; }
+
+        //what is inside, to determinate default expiration date
+        [DefaultValue( ProductType.Other )]
+        public ProductType Type { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+
+        //determinate which Quantity metric should be applied
+        [Required]
+        public QuantityType QuantityType { get; set; }
+        public int? QuantityCount { get; set; }
+        public float? QuantityWeight { get; set; }
+
+        public DateTime BuyDate { get; set; }
+
+        [DataType( DataType.Date )]
+        public DateTime? ExpirationDate { get; set; }
+
+        public string Comment { get; set; }
+
+    }
+}
