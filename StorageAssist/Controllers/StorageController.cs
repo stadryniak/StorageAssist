effcf915aea25bb4c5bd7de1870856ed542b5f3f
Michał Stadryniak <stadryniak.michal@gmail.com>
2019-10-05 18:17:53 +0200

    Add Storage frontend done

--- StorageAssist/Controllers/StorageController.cs
+++ StorageAssist/Controllers/StorageController.cs
@@ -8,15 +8,19 @@ namespace StorageAssist.Controllers
     {
         // GET
         [Authorize]
+        [ValidateAntiForgeryToken]
         public IActionResult Index()
         {
             return View();
         }
 
         [Authorize]
+        [ValidateAntiForgeryToken]
         [HttpPost]
-        public string AddStorage()
+        public string AddStorage(string storageName, string storageType, string commonResourceId)
         {
+            System.Diagnostics.Debug.WriteLine(storageName);
+            System.Diagnostics.Debug.WriteLine(storageType);
             return "Test";
             //return RedirectToAction("Index");
         }
