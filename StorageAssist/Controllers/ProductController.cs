50b1a9758fb14549038c92701d7b1d8b5ec005bd
Michał Stadryniak <stadryniak.michal@gmail.com>
2019-10-10 15:11:13 +0200

    Link to AddProduct controller and functions declarations add Progress is progress, no matter how small

---
+++ StorageAssist/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StorageAssist.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Storage");
+        }
+
+        public IActionResult AddProduct(string storageId)
+        {
+            System.Diagnostics.Debug.WriteLine(storageId);
+            return View();
+        }
+    }
+}
