23ce8401aa019f537dd1dd316fc1be31feca00a0
Michał Stadryniak <stadryniak.michal@gmail.com>
2019-12-04 00:47:31 +0100

    AddProductDb done View WIP

--- StorageAssist/Controllers/NotesController.cs
+++ StorageAssist/Controllers/NotesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -20,19 +21,46 @@ namespace StorageAssist.Controllers
             _user = user;
         }
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var common = _appUserContext.CommonResources.Where(c => c.OwnerId == _user.GetUserId(User))
-                .Include(c => c.Notes);
-            if (common == null)
+            var userCommons = await _appUserContext.UserCommonResources.Where(uc => uc.UserId == _user.GetUserId(User))
+                .Include(uc => uc.CommonResource)
+                    .ThenInclude(c => c.Notes).ToListAsync();
+            if (userCommons.Count == 0)
             {
                 var error = new ErrorViewModel()
                 {
-                    ErrorMessage = "Error 173: unable to get user's common resources"
+                    ErrorMessage = "Error 173: unable to get user's common resources. Possibly user don't have resources."
                 };
                 return RedirectToAction("Index", "Error", error);
             }
-            return View(common);
+
+            var commons = userCommons.Select(common => common.CommonResource).ToList();
+
+            return View(commons);
         }
+
+        [Authorize]
+        public async Task<IActionResult> AddNoteDb(string commonId, string noteName, string text)
+        {
+            var common = await _appUserContext.CommonResources.Where(c => c.CommonResourceId == commonId)
+                .FirstOrDefaultAsync();
+            Note note = new Note
+            {
+                CommonResourceId = commonId,
+                CommonResource = common,
+                OwnerId = _user.GetUserId(User),
+                NoteName = noteName,
+                NoteType = null,
+                NoteText = text
+            };
+            common.Notes.Add(note);
+            _appUserContext.Notes.Add(note);
+            _appUserContext.CommonResources.Update(common);
+            await _appUserContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
